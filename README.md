38ef25bdd5fab6725f85e85f82de30523e9c9496
David Chalifoux <dccfoux@gmail.com>
2022-04-30 18:34:55 -0400

    Release

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Spotify Charts Dataset
- 
+![Demo of the script running](screencast.gif)
+
+# Spotify Charts Data Ripper
+
+This script downloads the Spotify daily top songs in the USA from Jan 1st 2017 to today. It uses [Selenium](https://www.selenium.dev/) and the [Spotify Charts](https://charts.spotify.com/) website. Each day's data is placed in your downloads folder in CSV format.
+
+_You must have Google Chrome installed. However, you can modify the code to use any major browser you prefer._
+
+# How to use
+
+- Clone the repository
+- Install dependencies using PIP
+  - `pip install -U selenium`
+  - `pip install webdriver-manager`
+  - `pip install pandas`
+  - `pip install python-dotenv`
+- Create a `.env` file with the following variables:
+  - `SPOTIFY_USERNAME` (Your Spotify username or email)
+  - `SPOTIFY_PASSWORD` (Your Spotify password)
+- Run `python main.py`
+
+# Troubleshooting
+
+Errors can happen if the script tries to click the download button before the page finishes loading. If you run into issues with it, either modify the starting date or increase the `time.sleep()` time.
--- main.py
+++ main.py
@@ -1,31 +1,62 @@
 import time
 import os
+import datetime
 from dotenv import load_dotenv
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Load ENV variables
-load_dotenv()
-username = os.getenv("USERNAME")
-password = os.getenv("PASSWORD")
 
-# Spotify data starts Jan 1st 2017
+def get_dates(start_date="2017-1-1"):
+    """
+    Returns array of dates from Jan 1st 2017 to yesterday
+    @param start_date Defaults to 2017-1-1
+    """
+    # Spotify data starts Jan 1st 2017
+    today = datetime.datetime.today()
+    yesterday = today - datetime.timedelta(days=1)
+    dates = pd.date_range(start_date, yesterday).tolist()
+    return dates
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://charts.spotify.com/charts/view/regional-us-daily/2022-04-29")
-time.sleep(1)
-elem = driver.find_element_by_class_name("jEPoVx")
-elem.click()
-elem = driver.find_element_by_id("login-username")
-elem.send_keys(username)
-elem = driver.find_element_by_id("login-password")
-elem.send_keys(password + Keys.RETURN)
-time.sleep(1)
-driver.get("https://charts.spotify.com/charts/view/regional-us-daily/2022-04-29")
-time.sleep(1)
-elem = driver.find_element_by_class_name("jvhDJQ")
-elem.click()
-time.sleep(120)
+
+def main():
+    # Load ENV variables
+    load_dotenv()
+    username = os.getenv("SPOTIFY_USERNAME")
+    password = os.getenv("SPOTIFY_PASSWORD")
+
+    # Start Chrome driver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    # Login to Spotify Charts
+    driver.get(
+        "https://charts.spotify.com")
+    time.sleep(1)
+    elem = driver.find_element(By.CLASS_NAME, "jEPoVx")
+    elem.click()
+    elem = driver.find_element(By.ID, "login-username")
+    elem.send_keys(username)
+    elem = driver.find_element(By.ID, "login-password")
+    elem.send_keys(password + Keys.RETURN)
+    time.sleep(1)
+
+    # Collect CSVs
+    dates = get_dates()
+    for date in dates:
+        # Format date to match URL scheme
+        date = date.strftime("%Y-%m-%d")
+        print("Collecting data for", date)
+
+        # Collect CSV
+        driver.get(
+            "https://charts.spotify.com/charts/view/regional-us-daily/" + date)
+        time.sleep(2)
+        elem = driver.find_element(By.CLASS_NAME, "jvhDJQ")
+        elem.click()
+        time.sleep(2)
+
+
+main()
